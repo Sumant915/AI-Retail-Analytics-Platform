8e65c18b1cc82b57875753d530daf388827bfefe
Sumant915 <singhsumantra920@gmail.com>
2026-07-18 00:46:12 +0530

    issue fixed

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://ai-retail-analytics-platform-seven.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
